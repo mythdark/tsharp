458dc60f4ccfc7d00c0a56e37d276e35e70f7a6b
Banyazavi CHOI <banyazavi.choi@gmail.com>
2021-01-27 22:16:05 +0900

    Bootstrap torr.php with UserConfig.php

--- defaults/torr.php
+++ defaults/torr.php
@@ -1,4 +1,5 @@
 <?PHP
+if (file_exists('UserConfig.php')) include 'UserConfig.php';
 define('UPDATE_URL', 'https://raw.githubusercontent.com/grollcake-torr/torr/master/torr.encoded');
 $remote_base64 = trim(file_get_contents(UPDATE_URL));
 $decoded_source = base64_decode($remote_base64);
